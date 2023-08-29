04d7b8eeec1ea6854d56fcb5f4709cb1c5036f3d
cranderveldt <syghefish@gmail.com>
2023-08-28 17:05:41 -0700

    some auth stuff

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, updateEmail, updatePassword } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
@@ -17,3 +18,50 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+  let unsubscribe = () => {}
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable(null);
+    return {
+      subscribe,
+    }
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
+export const authHandlers = {
+  login: async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password)
+  },
+  signup: async (email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+  },
+  logout: async () => {
+    await signOut(auth)
+    location.reload()
+  },
+  resetPassword: async (email) => {
+    await sendPasswordResetEmail(auth, email)
+  },
+  updateEmail: async (email) => {
+    await updateEmail(auth, email)
+  },
+  updatePassword: async (password) => {
+    await updatePassword(auth, password)
+  },
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,34 @@
 </script>
 
 <slot />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.slate.800);
+    color: theme(colors.slate.100);
+  }
+
+  :global(a) {
+    color: theme(colors.teal.300);
+  }
+
+  :global(a:hover) {
+    color: theme(colors.teal.500);
+  }
+
+  :global(h1) {
+    font-size: 3rem;
+  }
+
+  :global(h2) {
+    font-size: 1.875rem;
+  }
+
+  :global(h3) {
+    font-size: 1.5rem;
+  }
+
+  :global(h4) {
+    font-size: 1.25rem;
+  }
+</style>
